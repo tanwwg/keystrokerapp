87cbb03d773b7a8e57794813f3d76fbd8c4e3e3d
Tan Thor Jen <tan@wisewizardgames.com>
2024-01-27 16:17:30 +0800

    Initial Commit

---
+++ keystroker/keystrokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  keystrokerApp.swift
+//  keystroker
+//
+//  Created by Tan Thor Jen on 27/1/24.
+//
+
+import SwiftUI
+
+@main
+struct keystrokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
